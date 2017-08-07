ffa7c4ac02e4262707057a01c95657c2fb18ab5c
Robert Schönthal <robert.schoenthal@gmail.com>
2017-08-07 23:30:03 +0200

    svg all the things

--- src/Icon/Icon.js
+++ src/Icon/Icon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Icon = props =>
-  <a href={props.target} target="_blank" className="icon">
+  <a href={props.target} target="_blank" rel="noopener" className="icon">
     <img src={props.src} alt={props.name} />
   </a>;
 
